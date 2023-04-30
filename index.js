44428e7cabca6d15300feecf635399b4b33d2ae8
PriyankaDashputre <dashputre.priyanka@gmail.com>
2023-04-30 11:57:22 -0700

    DOM Modify

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your code here!
+/* Part 1 */
+document.querySelector('main')
+
+main.remove()
+
+/* Part 2 */
+//const newHeader = document.querySelector('h1#victory');
+
+const newHeader = document.createElement('h1')
+
+newHeader.id = 'victory';
+//const id = document.createElementById.append('')
+
+/* Part 3 */
+//newHeader.textContent = "Priyanka is the champion";
+
+const victory = document.getElementById("victory");
+newHeader.innerHTML = "Priyanka is the champion";
